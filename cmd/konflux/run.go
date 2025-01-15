31c1b28e9f46ac6cccc4a390b618ed93a1781b4b
Vincent Demeester <vdemeest@redhat.com>
2025-01-15 16:23:42 +0100

    cmd/konflux: handle everything in one command
    It clones repositories, cleanup things, generates content, create
    branch, pushes and create pull-requests. All-in-one.
    
    Signed-off-by: Vincent Demeester <vdemeest@redhat.com>

---
+++ cmd/konflux/run.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+)
+
+func run(ctx context.Context, r string, name string, args ...string) ([]byte, error) {
+	var buf bytes.Buffer
+
+	select {
+	case <-ctx.Done():
+		return buf.Bytes(), ctx.Err()
+	default:
+	}
+
+	cmd := exec.Command(name, args...)
+
+	cmd.Dir = r
+	cmd.Stdout = io.MultiWriter(os.Stdout, &buf)
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		return nil, fmt.Errorf("[%s] failed to run %s %v: %w", r, name, args, err)
+	}
+	return buf.Bytes(), nil
+}
