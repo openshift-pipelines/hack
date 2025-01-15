f445f45571d81190dea77fa9a77640e5d6436af6
Vincent Demeester <vdemeest@redhat.com>
2025-01-15 17:08:15 +0100

    cmd/konflux: output commands and failure output
    Signed-off-by: Vincent Demeester <vdemeest@redhat.com>

--- cmd/konflux/run.go
+++ cmd/konflux/run.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"os/exec"
 )
@@ -18,14 +19,16 @@ func run(ctx context.Context, r string, name string, args ...string) ([]byte, er
 	default:
 	}
 
+	log.Println("Running", name, args, "in", r)
 	cmd := exec.Command(name, args...)
 
 	cmd.Dir = r
-	cmd.Stdout = io.MultiWriter(os.Stdout, &buf)
+	// cmd.Stdout = io.MultiWriter(os.Stdout, &buf)
+	cmd.Stdout = &buf
 	cmd.Stderr = os.Stderr
 
 	if err := cmd.Run(); err != nil {
-		return nil, fmt.Errorf("[%s] failed to run %s %v: %w", r, name, args, err)
+		return buf.Bytes(), fmt.Errorf("[%s] failed to run %s %v: %w", r, name, args, err)
 	}
 	return buf.Bytes(), nil
 }
